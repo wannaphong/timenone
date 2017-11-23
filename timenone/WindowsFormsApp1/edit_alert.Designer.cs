99e1471c4889af33c99f02ba2ecb953f63c83ff4
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-11-23 17:15:46 +0700

    add db

---
+++ timenone/WindowsFormsApp1/edit_alert.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class edit_alert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // edit_alert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 399);
+            this.Name = "edit_alert";
+            this.Text = "แก้ไขการแจ้งเตือน";
+            this.Load += new System.EventHandler(this.edit_alert_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ timenone/WindowsFormsApp1/edit_alert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class edit_alert : Form
+    {
+        public edit_alert()
+        {
+            InitializeComponent();
+        }
+
+        private void edit_alert_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
