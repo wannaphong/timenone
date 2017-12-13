d59ffbeb9f272762bb5fcba4bc1d38ebda76bc03
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-12-13 17:29:09 +0700

    update data

--- timenone3/timenone/edit_alert.Designer.cs
+++ timenone3/timenone/edit_alert.Designer.cs
@@ -38,6 +38,7 @@
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(3, 74);
             this.dataGridView1.Name = "dataGridView1";
--- timenone3/timenone/edit_alert.cs
+++ timenone3/timenone/edit_alert.cs
@@ -15,19 +15,19 @@ namespace timenone
         {
             InitializeComponent();
         }
-        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e) // เมื่อมีการคลิกแถวใดแถวหนึ่งทั้งแถว
         {
-            ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString()); // ให้นำค่า ID ไปเก็บในตัวแปร ID
         }
         public void DisplayData() // ประกาศ method ชื่อ DisplayData แบบ public โดยไม่มีคืนค่า
         {
             List<MyStruct> list = new List<MyStruct> { }; // ประกาศ List ชื่อ list ใช้เก็บวัตถุของ class ชื่อ MyStruct
-            var source = new BindingSource();
+            var source = new BindingSource(); // ประกาศ source แทน BindingSource เพื่อสำหรับไว้เพิ่มข้อมูลลงใน dataGridView1
             using (var db2 = new LiteDatabase(file)) // เรียกใช้งานฐานจาก LiteDatabase(ที่ตั้งไฟล์) แล้วเก็บวัตถุไว้ในตัวแปร db2
             {
-                var orders = db2.GetCollection<db.Notifications>("Notifications");
+                var orders = db2.GetCollection<db.Notifications>("Notifications"); // ประกาศตัวแปร orders แทนการประกาศการอ้างอิง Collection ชื่อ Notifications จากฐานข้อมูล
 
-                // When query Order, includes references
+                // ทำการ query กับ Order, เรียกใช้แทนการอ้างอิง
                 var query = orders
                     .Include(x => x.Id)
                     .Include(x => x.Title)
@@ -44,22 +44,23 @@ namespace timenone
                  .Find(x => x.IsActive == true || x.IsActive == false); // ค้นหาข้อมูล ถ้า IsActive เป็น true หรือ false
                 foreach (var c in query) // ลูปข้อมูลทีละตัวใน query เก้บไว้ใน c
                 {
-                    list.Add(new MyStruct(c.Id, c.Title, c.H, c.M, c.IsActive,c.Sunday,c.Monday,c.Tuesday,c.Wednesday,c.Thursday,c.Friday,c.Saturday));                }
+                    list.Add(new MyStruct(c.Id, c.Title, c.H, c.M, c.IsActive,c.Sunday,c.Monday,c.Tuesday,c.Wednesday,c.Thursday,c.Friday,c.Saturday)); // เพิ่มข้อมูลลง list
+                }
             }
-            source.DataSource = list;
+            source.DataSource = list; // กำหนดแหล่งข้อมูลของ source คือ list
             dataGridView1.DataSource = source; // กำหนดแหล่งข้อมูลของ dataGridView1 ให้เป็นตัวแปร source
             dataGridView1.Columns[0].ReadOnly = true; // กำหนดให้ Columns แรก (ID) ให้อ่านได้อย่างเดียว
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells; // กำหนดให้ปรับขนาด dataGridView1 แบบ Auto
 
             dataGridView1.BorderStyle = BorderStyle.None; // กำหนด dataGridView1 ไม่มีกรอบ
-            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(238, 239, 249);
-            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
-            dataGridView1.DefaultCellStyle.SelectionBackColor = Color.DarkTurquoise;
-            dataGridView1.DefaultCellStyle.SelectionForeColor = Color.WhiteSmoke;
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(238, 239, 249); // กำหนดสี AlternatingRowsDefaultCellStyle ตามรหัส RGB
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal; // กำหนดรูปแบบกรอบของ Cell ให้เป็นรูปแบบ SingleHorizontal
+            dataGridView1.DefaultCellStyle.SelectionBackColor = Color.DarkTurquoise; // กำหนดสีข้างหลังให้เป็นสี DarkTurquoise
+            dataGridView1.DefaultCellStyle.SelectionForeColor = Color.WhiteSmoke;  // กำหนดสีข้างหน้าให้เป็นสี  WhiteSmoke
             dataGridView1.BackgroundColor = Color.White; // กำหนดสีพื้นหลังใน dataGridView1 เป็น White
-            dataGridView1.EnableHeadersVisualStyles = false;
-            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
-            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(20, 25, 72);
+            dataGridView1.EnableHeadersVisualStyles = false; // ให้ปิดการทำงาน HeadersVisualStyles
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None; // กำหนดรูปแบบกรอกของหัว Column ให้ว่างเปล่า
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(20, 25, 72); // กำหนดสี ColumnHeadersDefaultCellStyle ตามรหัส RGB
             dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White; // กำหนดสีหัว Column เป็นสีขาว
         }
 
@@ -69,20 +70,20 @@ namespace timenone
         }
         class MyStruct // ประกาศ class ชื่อ MyStruct
         {
-            public int ID { get; set; }
-            public string Title { get; set; }
-            public string H { get; set; }
-            public string M { get; set; }
-            public bool IsActive { get; set; }
-            public bool Sunday { get; set; }
-            public bool Monday { get; set; }
-            public bool Tuesday { get; set; }
-            public bool Wednesday { get; set; }
-            public bool Thursday { get; set; }
-            public bool Friday { get; set; }
-            public bool Saturday { get; set; }
+            public int ID { get; set; } // ประกาศ ID
+            public string Title { get; set; } // ประกาศ Title
+            public string H { get; set; } // ประกาศ H
+            public string M { get; set; } // ประกาศ M
+            public bool IsActive { get; set; } // ประกาศ IsActive
+            public bool Sunday { get; set; } // ประกาศ Sunday
+            public bool Monday { get; set; } // ประกาศ Monday
+            public bool Tuesday { get; set; } // ประกาศ Tuesday
+            public bool Wednesday { get; set; } // ประกาศ Wednesday
+            public bool Thursday { get; set; } // ประกาศ Thursday
+            public bool Friday { get; set; } // ประกาศ Friday
+            public bool Saturday { get; set; } // ประกาศ Saturday
 
-            public MyStruct(int Id, string title, string h, string m, bool isActive,bool sunday,bool monday,bool tuesday,bool wednesday,bool thursday,bool friday,bool saturday)
+            public MyStruct(int Id, string title, string h, string m, bool isActive,bool sunday,bool monday,bool tuesday,bool wednesday,bool thursday,bool friday,bool saturday) // ประกาศ method ชื่อ  MyStruct ชื่อเดียวกับ class ที่ประกาศ
             {
                 ID = Id; // ให้ ID = Id
                 Title = title; // ให้ Title = title
@@ -99,26 +100,26 @@ namespace timenone
             }
         }
 
-        private void update_Click(object sender, EventArgs e)
+        private void update_Click(object sender, EventArgs e) // เมื่อกดปุ่ม update
         {
             using (var db2 = new LiteDatabase(file)) // เรียกใช้งานฐานจาก LiteDatabase(ที่ตั้งไฟล์) แล้วเก็บวัตถุไว้ในตัวแปร db2
             {
-                foreach (DataGridViewRow row in dataGridView1.Rows)
+                foreach (DataGridViewRow row in dataGridView1.Rows) // ลูปข้อมูลจาก dataGridView1.Rows ทีละตัวเก็บไว้ใน row
                 {
-                    var productsDb = db2.GetCollection<db.Notifications>("Notifications");
-                    var productToUpdate = productsDb.Find(p => p.Id == int.Parse(row.Cells[0].Value.ToString())).First();
+                    var productsDb = db2.GetCollection<db.Notifications>("Notifications"); // ประกาศตัวแปร productsDb แทนการประกาศการอ้างอิง Collection ชื่อ Notifications จากฐานข้อมูล
+                    var productToUpdate = productsDb.Find(p => p.Id == int.Parse(row.Cells[0].Value.ToString())).First(); // ค้นหาและดึงข้อมูลโดยอ้างอิงตาม ID แล้วเก็บข้อมูลไว้ใน productToUpdate
                     productToUpdate.Title = row.Cells[1].Value.ToString(); // Title
-                    productToUpdate.H= row.Cells[2].Value.ToString();
-                    productToUpdate.M=row.Cells[3].Value.ToString();
-                    productToUpdate.IsActive= bool.Parse(row.Cells[4].Value.ToString());
-                    productToUpdate.Sunday = bool.Parse(row.Cells[5].Value.ToString());
-                    productToUpdate.Monday = bool.Parse(row.Cells[6].Value.ToString());
-                    productToUpdate.Tuesday = bool.Parse(row.Cells[7].Value.ToString());
-                    productToUpdate.Wednesday = bool.Parse(row.Cells[8].Value.ToString());
-                    productToUpdate.Thursday = bool.Parse(row.Cells[9].Value.ToString());
-                    productToUpdate.Friday = bool.Parse(row.Cells[10].Value.ToString());
-                    productToUpdate.Saturday = bool.Parse(row.Cells[11].Value.ToString());
-                    productsDb.Update(productToUpdate);
+                    productToUpdate.H= row.Cells[2].Value.ToString(); // H
+                    productToUpdate.M=row.Cells[3].Value.ToString();// M
+                    productToUpdate.IsActive= bool.Parse(row.Cells[4].Value.ToString()); // IsActive
+                    productToUpdate.Sunday = bool.Parse(row.Cells[5].Value.ToString());// Sunday
+                    productToUpdate.Monday = bool.Parse(row.Cells[6].Value.ToString());// Monday
+                    productToUpdate.Tuesday = bool.Parse(row.Cells[7].Value.ToString());// Tuesday
+                    productToUpdate.Wednesday = bool.Parse(row.Cells[8].Value.ToString());// Wednesday
+                    productToUpdate.Thursday = bool.Parse(row.Cells[9].Value.ToString()); //Thursday
+                    productToUpdate.Friday = bool.Parse(row.Cells[10].Value.ToString());//Friday
+                    productToUpdate.Saturday = bool.Parse(row.Cells[11].Value.ToString());//Saturday
+                    productsDb.Update(productToUpdate); // อัพเดตข้อมูล
                 }
                 MessageBox.Show("อัพเดตข้อมูลเรียบร้อย"); // โชว์ MessageBox ว่า "อัพเดตข้อมูลเรียบร้อย"
                 DisplayData(); // เรียกใช้ method ชื่อ DisplayData
@@ -127,20 +128,20 @@ namespace timenone
 
         private void del_Click(object sender, EventArgs e) // เมื่อคลิกปุ่ม del
         {
-            using (var db2 = new LiteDatabase(file))
+            using (var db2 = new LiteDatabase(file)) // เรียกใช้ฐานข้อมูล LiteDatabase โดยใช้ path ที่ได้จากตัวแปร file
             {
-                var orders = db2.GetCollection<db.Notifications>("Notifications");
+                var orders = db2.GetCollection<db.Notifications>("Notifications"); // ประกาศตัวแปร orders แทนการประกาศการอ้างอิง Collection ชื่อ Notifications จากฐานข้อมูล
 
-                // When query Order, includes references
-                var query = orders//.Include(S => timeOfDay.Seconds.ToString())
+                // ทำการ query กับ Order, เรียกใช้แทนการอ้างอิง
+                var query = orders
                     .Include(x => x.IsActive)
-                 .Find(x => x.IsActive == true || x.IsActive == false);
-                orders.Delete(Query.EQ("_id", ID));
+                 .Find(x => x.IsActive == true || x.IsActive == false); // ค้นหาข้อมูล IsActive ถ้าเป็น true หรือ false
+                orders.Delete(Query.EQ("_id", ID)); // ลบข้อมูลตาม ID ที่อ้างอิง
             }
             DisplayData(); // เรียกใช้ method ชื่อ DisplayData
         }
 
-        private void add_alert_bn_Click(object sender, EventArgs e)
+        private void add_alert_bn_Click(object sender, EventArgs e) // เมื่อคลิกปุ่ม
         {
             add_alert a = new add_alert(); // ประกาศตัวแปร a แทนวัตถุ add_alert
             a.ShowDialog(); // กำหนดให้โชว์แบบ ShowDialog คือ โชว์ได้หน้าต่างเดียวและใช้งานโปรแกรมได้เฉพาะหน้าต่างที่โชว์อยู่
