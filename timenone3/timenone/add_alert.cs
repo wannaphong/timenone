a7a67083f60b4037f216859d1715f2ee3e82c4d0
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-12-13 15:50:05 +0700

    ok add_alert

--- timenone3/timenone/add_alert.cs
+++ timenone3/timenone/add_alert.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace timenone
@@ -20,12 +13,12 @@ namespace timenone
 
         private void clean_Click(object sender, EventArgs e) // เมื่อคลิกปุ่ม clean
         {
-            h.SelectedIndex = 0;
-            m.SelectedIndex = 0;
+            h.SelectedIndex = 0; // ให้ h เลือกที่ 0
+            m.SelectedIndex = 0; // ให้ m เลือกที่ 0
             note.Text = ""; // กำหนดให้ข้อความใน note ให้ว่างเปล่า
-            foreach (int i in days.CheckedIndices)
+            foreach (int i in days.CheckedIndices) // ลูปค่าจำนวนใน days แต่ละตัวเลือก
             {
-                days.SetItemCheckState(i, CheckState.Unchecked);
+                days.SetItemCheckState(i, CheckState.Unchecked); // ให้ยกเลิกการเลือก
             }
             Enable.Checked = false; // ให้ยกเลิกการเลือกของ Enable
         }
@@ -42,12 +35,12 @@ namespace timenone
             var db = new db();
             string[] d = {"วันอาทิตย์","วันจันทร์","วันอังคาร","วันพุธ","วันพฤหัสบดี","วันศุกร์","วันเสาร์"}; // ประกาศ string[] ชื่อ d แทนวันทั้ง 7 วันในภาษาไทย
             bool d1=false, d2 = false, d3 = false, d4 = false, d5 = false, d6 = false, d7 = false; // ประกาศตัวแปรชนิด bool ชื่อ d1 - d7 ให้มีค่าเริ่มต้นเป็น false
-            foreach (string item in days.CheckedItems)
+            foreach (string item in days.CheckedItems) // ลูปเอาค่า string ทีละตัวจาก days.CheckedItems เก็บไว้ใน item
             {
                 int i = 0; // ประกาศตัวแปรชนิด int ชื่อ i มีค่าเริ่มต้นเป็น 0
                 while (i < d.Length) // ลูป while ถ้า i น้อยกว่าความยาวของ d
                 {
-                    if (d[i] == item)
+                    if (d[i] == item) // ถ้า d ที่ i เท่ากับ item
                     {
                         if (i == 0) d1 = true; // ถ้า i เท่ากับ 0 ให้ d1 เป็น true
                         else if (i == 1) d2 = true; // ถ้า i เท่ากับ 1 ให้ d2 เป็น true
